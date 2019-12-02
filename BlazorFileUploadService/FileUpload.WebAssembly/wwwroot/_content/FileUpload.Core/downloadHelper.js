7e09e391d97d4f69a770f87b259b3db65e32c1ac
SeppPenner <HansM2013@gmx.de>
2019-12-02 15:50:17 +0100

    Adjusted icon and heading.

---
+++ BlazorFileUploadService/FileUpload.WebAssembly/wwwroot/_content/FileUpload.Core/downloadHelper.js
@@ -0,0 +1,21 @@
+﻿window.downloadHelper = {
+    downloadFile: function (data, fileName, mimeType) {
+        const link = document.createElement("a");
+        const blob = window.downloadHelper.base64ToBlob(data, mimeType);
+        link.href = window.URL.createObjectURL(blob);
+        link.download = fileName;
+        link.dispatchEvent(new MouseEvent(`click`));
+    },
+    base64ToBlob: function (base64, mimeType) {
+        const binary = atob(base64.replace(/\s/g, ""));
+        const len = binary.length;
+        const buffer = new ArrayBuffer(len);
+        const view = new Uint8Array(buffer);
+
+        for (let i = 0; i < len; i++) {
+            view[i] = binary.charCodeAt(i);
+        }
+
+        return new Blob([view], { type: mimeType });
+    }
+};
